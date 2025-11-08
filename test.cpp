0bab7f1c584b98709f16c93860295f28dbce23cd
junayedahamed <junayedahamed660@gmail.com>
2025-11-08 19:54:15 +0600

    test

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@ int main() {
     cin >> x >> y;
     cout<<x+y<<endl;
     cout<<x-y<<endl;
-    cout<<x*y<<endl;
+    // cout<<x*y<<endl;
     cout<<x/y<<endl;
     cout << "Hello, World!" << endl;
     return 0;
